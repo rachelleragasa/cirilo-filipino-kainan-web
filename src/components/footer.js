83901cd9f6d2cc8770d82b29c4cdfe5a3f05963a
rachelle ragasa <rachelleragasa.costa@gmail.com>
2020-07-15 14:21:45 +0100

    fix(footer): reducing font size for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { above, color, greyBackground, semplicitaProMedium } from "../styles"
 const Footer = () => (
   <Container>
     <FooterInfo>
-      <div>
+      <FooterColumn>
         <h3>Where are we?</h3>
         <p>
           4 Cable St,
@@ -14,17 +14,17 @@ const Footer = () => (
           <br />
           London E1 8J
         </p>
-      </div>
-      <div>
+      </FooterColumn>
+      <FooterColumn>
         <h3>Hours</h3>
         <p>Mon - Tues / closed</p>
         <p>Thurs - Sun / 5pm - 10pm</p>
-      </div>
-      <div>
+      </FooterColumn>
+      <FooterColumn>
         <h3>Contact Us</h3>
         <p>020 7702 2299</p>
         <p>eat@cirilofilipinokainan.co.uk</p>
-      </div>
+      </FooterColumn>
     </FooterInfo>
     <div>© {new Date().getFullYear()} Cirilo Filipino Kainan</div>
   </Container>
@@ -34,11 +34,15 @@ const Container = styled.footer`
   height: auto;
   padding: 40px 20px 20px 20px;
   text-transform: uppercase;
-  font-size: 14px;
+  font-size: 12px;
   letter-spacing: 2px;
   color: ${color.white};
   ${greyBackground};
   text-align: center;
+
+  ${above.mobile`
+    font-size: 14px;
+  `}
 
   ${above.tabletLarge`
     padding: 40px 40px 20px 40px;
@@ -64,9 +68,17 @@ const FooterInfo = styled.div`
   }
 
   p {
-    font-size: 14px;
+    font-size: 12px;
     line-height: 20px;
+
+    ${above.mobile`
+      font-size: 14px;
+    `}
   }
+`
+
+const FooterColumn = styled.div`
+  margin-bottom: 20px;
 `
 
 export default Footer
